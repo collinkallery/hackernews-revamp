ac51c3a7d91c9ceddae1b8eac9d7cecfa026e085
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-27 11:04:36 -0600

    Create file blueprint

---
+++ src/Components/ArticlePreview/ArticlePreview.js
@@ -0,0 +1,11 @@
+import  React from 'react';
+import './ArticlePreview.css';
+import ArticleExpanded from '../ArticleExpanded/ArticleExpanded'
+
+const ArticlePreview = () => {
+  return (
+    ''
+  )
+}
+
+export default ArticlePreview;
---
+++ src/Components/ArticlePreview/ArticlePreview.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ArticlePreview from './ArticlePreview'
+import {render} from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+
+describe('Article Preview', () => {
+
+  it('should do something', () => {
+    
+  })
+})
