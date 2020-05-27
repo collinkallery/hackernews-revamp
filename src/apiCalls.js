911e209df457271321fbc026ecb4fbb85c246412
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-27 10:31:33 -0600

    Create dynamic fetch method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,7 @@
-export const fetchPromises = () => {
-  
+export const fetchPromises = async (keyword) => {
+  const response = await fetch(`https://hacker-news.firebaseio.com/v0/${keyword}.json?print=pretty`)
+  const data = response.json()
+  return data
 }
 
 //change name of method
