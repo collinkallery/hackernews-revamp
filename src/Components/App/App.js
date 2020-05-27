b803a3ebbe753f973ccdc55b3fb4f8d18748745c
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-27 11:21:26 -0600

    Populate homePageStories in state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import {fetchPromises} from '../../apiCalls'
+import {fetchPromises, fetchStories} from '../../apiCalls'
+import ArticleContainer from '../ArticleContainer/ArticleContainer'
 
 class App extends Component {
   constructor() {
@@ -8,22 +9,31 @@ class App extends Component {
     this.state ={
       newStoryIDs: [],
       bestStoryIDs: [],
-      topStoryIDs: []
+      topStoryIDs: [],
+      homePageStories: []
     }
   }
 
   componentDidMount = () => { 
     fetchPromises('newstories')
       .then(data => this.setState({newStoryIDs: data}))
+      .then(newData => this.getStories(this.state.newStoryIDs[0]))
       .catch(err => console.error(err))
 
     fetchPromises('beststories')
       .then(data => this.setState({bestStoryIDs: data}))
+      .then(newData => this.getStories(this.state.bestStoryIDs[0]))
       .catch(err => console.error(err))
 
     fetchPromises('topstories')
       .then(data => this.setState({topStoryIDs: data}))
+      .then(newData => this.getStories(this.state.topStoryIDs[0]))
       .catch(err => console.error(err))
+  }
+
+  getStories = async (id) => {
+    const story = await fetchStories(id)
+    this.setState({homePageStories: [...this.state.homePageStories, story]})
   }
 
 
@@ -35,6 +45,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <ArticleContainer />
       </div>
     );
   }
