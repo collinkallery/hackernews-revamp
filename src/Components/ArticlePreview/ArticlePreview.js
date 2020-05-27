7fb0a47a2340d1dcd93bfccf3c6b35ebc9e05ccf
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-27 12:02:45 -0600

    Add StoryCardStyledComponent

--- src/Components/ArticlePreview/ArticlePreview.js
+++ src/Components/ArticlePreview/ArticlePreview.js
@@ -1,10 +1,20 @@
 import  React from 'react';
 import './ArticlePreview.css';
 import ArticleExpanded from '../ArticleExpanded/ArticleExpanded'
+import styled from 'styled-components'
 
-const ArticlePreview = () => {
+const StoryCard = styled.div`
+  background-color: purple;
+  width: 75%;
+  height: 25%;
+`
+
+const ArticlePreview = (props) => {
+
   return (
-    ''
+      <StoryCard>
+        <p>{props.title}</p>
+      </StoryCard>  
   )
 }
 
