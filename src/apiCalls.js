4d4a41d95433b433ddd38bb639db49a35b0ed5c6
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-27 11:04:56 -0600

    Create fetchStories method

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,4 +4,10 @@ export const fetchPromises = async (keyword) => {
   return data
 }
 
+export const fetchStories = async (storyID) => {
+  const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${storyID}.json?print=pretty`)
+  const data = response.json()
+  return data
+}
+
 //change name of method
