a488056af89ec74a84f006ff23d82c7b3be46871
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-26 20:04:18 -0600

    Convert into class component, import apiCalls

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import fetchPromises from '../../apiCalls'
 
-const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state ={}
+  }
+
+  fetchData = () => {
+    fetchPromises('desired fetch go here')
+    //This may be in a different file such as ArticleContainer... Not entirely sure
+  }
+  
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
