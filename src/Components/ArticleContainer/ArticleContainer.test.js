edcddd114f25e397d8337b5f5e37ffdb7727c20b
collinkallery <collin.kallery@colorado.edu>
2020-05-28 16:41:33 -0600

    Delete files

--- src/Components/ArticleContainer/ArticleContainer.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ArticleContainer from "./ArticleContainer";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-
-describe("Article Container", () => {
-  it("should do something", () => {});
-});
