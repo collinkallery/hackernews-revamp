8faafbb29fa1edcaa641c3069ac1985e8c73fc17
“Lauren <lucerolaurenm@gmail.com>
2020-05-30 14:06:50 -0400

    Fix merge conflix

---
+++ src/Components/AllPreviewContainer/AllPreviewContainer.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import {fetchStoriesTest} from '../../apiCalls'
+
+const AllPreviewContainer = (props) => {
+    return (
+      <p>Hi</p>
+    );
+  }
+
+export default AllPreviewContainer;
