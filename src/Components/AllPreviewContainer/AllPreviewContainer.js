d419933fae217a3c336e5410f5260018e51c6ce5
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-30 10:44:45 -0600

    Create blueprint layout of Preview container

--- src/Components/AllPreviewContainer/AllPreviewContainer.js
+++ src/Components/AllPreviewContainer/AllPreviewContainer.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
+import {fetchStoriesTest} from '../../apiCalls'
 
-const AllPreviewContainer = () => {
-  console.log('I am here')
-  return "";
-};
+const AllPreviewContainer = (props) => {
+    return (
+      <p>Hi</p>
+    );
+  }
 
 export default AllPreviewContainer;
