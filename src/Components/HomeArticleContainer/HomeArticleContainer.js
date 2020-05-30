5cd2460fd1e45e5942d038aea9aae92e25c56372
“Lauren <lucerolaurenm@gmail.com>
2020-05-30 14:05:58 -0400

    Error handling

--- src/Components/HomeArticleContainer/HomeArticleContainer.js
+++ src/Components/HomeArticleContainer/HomeArticleContainer.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ArticlePreview from "../ArticlePreview/ArticlePreview";
 import styled from "styled-components";
-import {darkTheme} from "../../theme/globalStyle";
+import { darkTheme } from "../../theme/globalStyle";
 
-const {primaryPurple, primaryBlue, secondaryTeal, background, textColor, error} = darkTheme;
+const {
+  primaryPurple,
+  primaryBlue,
+  secondaryTeal,
+  background,
+  textColor,
+  error,
+} = darkTheme;
 
 const Wrapper = styled.div`
   display: flex;
@@ -13,9 +20,8 @@ const Wrapper = styled.div`
   flex-wrap: wrap;
   width: 100%;
   height: 100%;
-  border: 2px solid black;
   background-color: ${background};
-`
+`;
 
 const ArticleWrapper = styled.section`
   background-color: ${background};
@@ -32,23 +38,22 @@ const ArticleWrapper = styled.section`
     padding: 0;
     border-bottom: 2px solid ${secondaryTeal};
   }
-`
+`;
 
 const HomeArticleContainer = (props) => {
-
-  const articlesToDisplay = props.homePageStories.map(story => {
+  const [rerender, setRerender] = useState("");
+  const articlesToDisplay = props.homePageStories.map((story) => {
+    if (!story) {
+      setRerender(story);
+    }
     return (
       <ArticleWrapper>
         <h3>{story.topic} Story</h3>
-        <ArticlePreview {...story} key={story.id}/>
+        <ArticlePreview {...story} key={story.id} />
       </ArticleWrapper>
-    )
-  })
-  return (
-    <Wrapper>
-      {articlesToDisplay}
-    </Wrapper>
-  )
-}
+    );
+  });
+  return <Wrapper>{articlesToDisplay}</Wrapper>;
+};
 
 export default HomeArticleContainer;
