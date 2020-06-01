15172a634e550e3166680e320e81bd146b5c719b
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-01 17:43:56 -0600

    Add base test for article preview

--- src/Components/ArticlePreview/ArticlePreview.test.js
+++ src/Components/ArticlePreview/ArticlePreview.test.js
@@ -1,9 +1,46 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM  from "react-dom";
 import ArticlePreview from "./ArticlePreview";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import { fetchImage } from "../../apiCalls";
+import { MemoryRouter, Link } from 'react-router-dom'
 import "@testing-library/jest-dom/extend-expect";
 
+// jest.mock('../../apiCalls')
+
+//put in it block 
+// fetchImage.mockResolvedValueOnce(imageData)
+
 describe("Article Preview", () => {
-  it("should do something", () => {});
+
+  const previewData = {
+    id: 23381156,
+    title: "Debian Handbook for Debian 10 Buster",
+    topic: "/articles/Top",
+    url: "https://debian-handbook.info/browse/stable/",
+  }
+
+  const imageData = {
+    hybridGraph: {
+      image: "https://debian-handbook.info/browse/stable/Common_Content/images/image_left.png",
+      description: 'Random'
+    }
+  }
+
+  it("should render without crashing", () => {
+    const { getByText } = render(<MemoryRouter><ArticlePreview {...previewData}/></MemoryRouter>)
+    const title = getByText(previewData.title);
+    expect(title).toBeInTheDocument();
+  });
+
+  // it('image should be in the document', () => {
+  //     fetchImage.mockResolvedValueOnce(previewData)
+  //     const { getByAltText } = render(<MemoryRouter><ArticlePreview {...previewData}/></MemoryRouter>)
+
+  //     // const setClickedArticleMock = jest.fn()
+
+  //     const image = getByAltText(/Some random description/i);
+  //     expect(image).toBeInTheDocument();
+
+  // })
 });
