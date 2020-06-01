bf809eb68227850fd77f137f6c251509a9aa0f0b
Charlie Bandstra <bandstracharlie@gmail.com>
2020-06-01 14:02:49 -0600

    Create blueprint to print out data

--- src/Components/ArticleExpanded/ArticleExpanded.js
+++ src/Components/ArticleExpanded/ArticleExpanded.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const ArticleExpanded = () => {
-  return "";
+const ArticleExpanded = (props) => {
+  return (
+  <div>
+    <p>{props.clickedArticle.description}</p>
+  </div>
+  )
 };
 
 export default ArticleExpanded;
