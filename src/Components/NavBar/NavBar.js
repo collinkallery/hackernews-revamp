50a95c8c8d306a72402b73517025fa90f31817d9
“Lauren <lucerolaurenm@gmail.com>
2020-06-02 08:35:48 -0400

    Switch NavBar order to match hompage stories

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -79,8 +79,8 @@ const NavBar = () => {
       <NavBarBottomStyled>
         <TopicLinkStyled to="/articles/About">About</TopicLinkStyled>
         <TopicLinkStyled to="/articles/Newest">New</TopicLinkStyled>
-        <TopicLinkStyled to="/articles/Top">Top</TopicLinkStyled>
         <TopicLinkStyled to="/articles/Best">Best</TopicLinkStyled>
+        <TopicLinkStyled to="/articles/Top">Top</TopicLinkStyled>
         <TopicLinkStyled to="/articles/Saved">Saved</TopicLinkStyled>
       </NavBarBottomStyled>
     </NavBarStyled>
