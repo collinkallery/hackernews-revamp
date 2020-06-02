8bd503ab88484f527d7b6352097d7b3cbece1536
collinkallery <collin.kallery@colorado.edu>
2020-06-02 10:17:27 -0600

    Add in map over saved articles to return article previews and render them

--- src/Components/SavedContainer/SavedContainer.js
+++ src/Components/SavedContainer/SavedContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import {darkTheme} from "../../theme/globalStyle";
+import ArticlePreview from '../ArticlePreview/ArticlePreview';
 
 const {
   primaryPurple,
@@ -11,9 +12,44 @@ const {
   error
 } = darkTheme;
 
-const SavedContainer = () => {
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+  background-color: ${background};
+`
+
+const ArticleWrapper = styled.section`
+  background-color: ${background};
+  width: 90%;
+  height: 25%;
+  display: flex;
+  flex-direction: column;
+  padding: 2%;
+  border-top: 1px solid ${secondaryTeal};
+  /* border-bottom: 1px solid ${secondaryTeal}; */
+  margin: 3% 1%;
+`
+
+const SavedContainer = (props) => {
+  const articlesToDisplay = () => {
+    let articles = props.savedArticles.map(article => {
+      return (
+        <ArticleWrapper>
+          <ArticlePreview {...article} key={article.id} />
+        </ArticleWrapper>
+      )
+    })
+    return articles;
+  }
   return (
-    <p>SavedContainer Mounted</p>
+    <Wrapper>
+      {articlesToDisplay()}
+    </Wrapper>
   )
 }
 
