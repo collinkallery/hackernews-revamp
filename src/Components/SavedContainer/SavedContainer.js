a5a05dfcc891fc4396c4da749f81f98038bccfb5
collinkallery <collin.kallery@colorado.edu>
2020-06-02 10:05:08 -0600

    Create component for saved container

---
+++ src/Components/SavedContainer/SavedContainer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import {darkTheme} from "../../theme/globalStyle";
+
+const {
+  primaryPurple,
+  primaryBlue,
+  secondaryTeal,
+  background,
+  textColor,
+  error
+} = darkTheme;
+
+const SavedContainer = () => {
+  return (
+    <p>SavedContainer Mounted</p>
+  )
+}
+
+export default SavedContainer;
