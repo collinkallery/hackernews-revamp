ebecbe9ce4e48b2b0922a8ae12f39cf636ff3b28
collinkallery <collin.kallery@colorado.edu>
2020-06-02 09:35:32 -0600

    Styling of article expanded with title, description, image, and buttons

--- src/Components/ArticleExpanded/ArticleExpanded.js
+++ src/Components/ArticleExpanded/ArticleExpanded.js
@@ -1,10 +1,81 @@
 import React from "react";
+import styled from "styled-components";
+import {darkTheme} from "../../theme/globalStyle";
+
+const {
+  primaryPurple,
+  primaryBlue,
+  secondaryTeal,
+  background,
+  textColor,
+  error
+} = darkTheme;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+  background-color: ${background};
+  padding: 3%;
+
+  img {
+    width: 100%;
+  }
+
+  h2 {
+    font-size: 1.6em;
+    padding: 3%;
+  }
+
+  p {
+    padding: 3%;
+  }
+
+  button {
+    width: 40%;
+    background-color: ${secondaryTeal};
+    margin: 5%;
+    border: none;
+    padding: 3%;
+    font-size: .8em;
+    border-radius: 5px;
+  }
+
+  button:hover {
+    background-color: ${primaryBlue};
+    color: #FFFFFF;
+  }
+`
+const ImgContainer = styled.section`
+  width: 90%;
+  align-self: center;
+`
+
+const BtnContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+
+`
 
 const ArticleExpanded = (props) => {
+  console.log('hi', props);
   return (
-  <div>
+  <Wrapper>
+    <h2>{props.clickedArticle.title}</h2>
+    <ImgContainer>
+      <img src={props.clickedArticle.image}/>
+    </ImgContainer>
     <p>{props.clickedArticle.description}</p>
-  </div>
+    <BtnContainer>
+      <button>View Whole Article</button>
+      <button>Save this Article</button>
+    </BtnContainer>
+  </Wrapper>
   )
 };
 
