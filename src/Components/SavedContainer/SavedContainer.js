5ce1fca7d87c592a8639e6ed722e12ef19123224
“Lauren <lucerolaurenm@gmail.com>
2020-06-03 13:16:38 -0400

    Update darkTheme, run Prettier

--- src/Components/SavedContainer/SavedContainer.js
+++ src/Components/SavedContainer/SavedContainer.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import {darkTheme} from "../../theme/globalStyle";
-import ArticlePreview from '../ArticlePreview/ArticlePreview';
+import { darkTheme } from "../../theme/globalStyle";
+import ArticlePreview from "../ArticlePreview/ArticlePreview";
 
 const {
   primaryPurple,
   primaryBlue,
   secondaryTeal,
   background,
-  textColor,
-  error
+  surface,
+  error,
 } = darkTheme;
 
 const Wrapper = styled.div`
@@ -21,7 +21,7 @@ const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   background-color: ${background};
-`
+`;
 
 const ArticleWrapper = styled.section`
   background-color: ${background};
@@ -31,31 +31,25 @@ const ArticleWrapper = styled.section`
   flex-direction: column;
   padding: 2%;
   border-top: 1px solid ${secondaryTeal};
-  /* border-bottom: 1px solid ${secondaryTeal}; */
   margin: 3% 1%;
-`
+`;
 
 const SavedContainer = (props) => {
-
   const innerText = () => {
     if (props.savedArticles.length > 0) {
-      let articles = props.savedArticles.map(article => {
+      let articles = props.savedArticles.map((article) => {
         return (
           <ArticleWrapper>
             <ArticlePreview {...article} key={article.id} />
           </ArticleWrapper>
-        )
-      })
-    return articles;
-  } else {
-    return <h2>You have not saved any articles yet!</h2>
-  }
-}
-  return (
-    <Wrapper>
-      {innerText()}
-    </Wrapper>
-  )
-}
+        );
+      });
+      return articles;
+    } else {
+      return <h2>You have not saved any articles yet!</h2>;
+    }
+  };
+  return <Wrapper>{innerText()}</Wrapper>;
+};
 
 export default SavedContainer;
