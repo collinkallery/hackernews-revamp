bb4ebaab19ccba687818e83164bcb3fe701a4710
“Lauren <lucerolaurenm@gmail.com>
2020-06-03 17:34:51 -0400

    Unit testing

--- src/Components/ArticleExpanded/ArticleExpanded.test.js
+++ src/Components/ArticleExpanded/ArticleExpanded.test.js
@@ -1,9 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ArticleExpanded from "./ArticleExpanded";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { MemoryRouter } from "react-router-dom";
 
-describe("Article Expanded", () => {
-  it("should do something", () => {});
+describe("ArticleExpanded", () => {
+  let story1 = {
+    by: "colinprince",
+    descendants: 0,
+    id: 23404593,
+    image:
+      "https://d2r55xnwy6nx47.cloudfront.net/uploads/2020/06/Dimitrov_Blog_1200_Social.jpg",
+    description:
+      "Mathematicians try to figure out when problems can be solved using current knowledge — and when they have to chart a new path instead.",
+    score: 1,
+    time: 1591198783,
+    title: "Amazon's unrivalled power threatens jobs, communities",
+    type: "story",
+    url:
+      "https://www.cbc.ca/radio/thesundayedition/the-sunday-edition-for-may-31-2020-1.5580246/amazon-s-unrivalled-power-threatens-jobs-communities-and-democracy-monopoly-critic-stacy-mitchell-1.5585645",
+  };
+
+  it("should render ArticleExpanded", () => {
+    const { getByAltText } = render(
+      <MemoryRouter>
+        <ArticleExpanded clickedArticle={story1} />
+      </MemoryRouter>
+    );
+
+    expect(getByAltText(story1.description)).toBeInTheDocument();
+  });
+
+  it("should render buttons", () => {
+    const mockUpdateSaved = jest.fn();
+    const { getByText } = render(
+      <MemoryRouter>
+        <ArticleExpanded
+          clickedArticle={story1}
+          updateSavedArticles={mockUpdateSaved}
+        />
+      </MemoryRouter>
+    );
+    const readArticleBtn = getByText("Read Article");
+    const saveArticleBtn = getByText("Save this Article");
+    expect(readArticleBtn).toBeInTheDocument();
+    fireEvent.click(saveArticleBtn);
+    expect(mockUpdateSaved).toHaveBeenCalled();
+  });
 });
--- src/Components/ArticlePreview/ArticlePreview.test.js
+++ src/Components/ArticlePreview/ArticlePreview.test.js
@@ -1,48 +1,58 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 import ArticlePreview from "./ArticlePreview";
-import { render, fireEvent, getByTestId, waitFor } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  getByTestId,
+  waitFor,
+} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { fetchImage } from "../../apiCalls";
-import { MemoryRouter } from 'react-router-dom'
-
-//put in it block 
-// fetchImage.mockResolvedValueOnce(imageData)
+import { MemoryRouter } from "react-router-dom";
 
 describe("Article Preview", () => {
-
-  
-
   const previewData = {
     id: 23381156,
     title: "Debian Handbook for Debian 10 Buster",
     topic: "/articles/Top",
     url: "https://debian-handbook.info/browse/stable/",
-  }
+  };
 
   const imageData = {
     hybridGraph: {
-      image: "https://debian-handbook.info/browse/stable/Common_Content/images/image_left.png",
-      description: 'Random'
-    }
-  }
+      image:
+        "https://debian-handbook.info/browse/stable/Common_Content/images/image_left.png",
+      description: "Random",
+    },
+  };
 
   it("should render without crashing", () => {
-    const { getByText } = render(<MemoryRouter><ArticlePreview set{...previewData}/></MemoryRouter>)
+    const { getByText } = render(
+      <MemoryRouter>
+        <ArticlePreview set {...previewData} />
+      </MemoryRouter>
+    );
 
     const title = getByText(previewData.title);
 
     expect(title).toBeInTheDocument();
   });
 
-
   it("should render without crashing", async () => {
     const mockHandleClickedArticle = jest.fn();
     const mockSetClickedArticle = jest.fn();
-    const { getByTestId } = render(<MemoryRouter><ArticlePreview setClickedArticle={mockSetClickedArticle} {...previewData}/></MemoryRouter>)
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <ArticlePreview
+          setClickedArticle={mockSetClickedArticle}
+          {...previewData}
+        />
+      </MemoryRouter>
+    );
 
     const link = await getByTestId(previewData.id);
-    
+
     fireEvent.click(link);
 
     expect(mockHandleClickedArticle).toHaveBeenCalled();
